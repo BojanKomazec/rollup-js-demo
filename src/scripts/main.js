a42d2a79532c13b555ff581033e14714c16d1085
Bojan Komazec <bojan.komazec@gmail.com>
2018-09-18 13:59:40 +0100

    Added App code and required packages for the initial demo (running the bundle in Node terminal).

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+// application entry point
+import foo from "./modules/foo.js";
+export default function() {
+    console.log(foo);
+}
---
+++ src/scripts/modules/foo.js
@@ -0,0 +1,4 @@
+// a module
+
+// export a primitive (string in this case)
+export default 'hello world!';
